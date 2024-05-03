312c04f975579d029bed0d50a6d59244b397dba6
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-05-03 17:41:54 +0530

    add default files and done with config app

---
+++ server.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.DB_URL);
+const db=mongoose.connection;  //to check if connection is successful
+
+db.on('error',(error) => console.log("error"));  
+db.once('open',() => console.log("database connected successfully"));  
+
+
+app.listen(3000,() =>{
+    console.log("server started on port 3000")
+});
+
+app.use(express.json())
+
+const subscribersRouter=require('./routes/subscribers');
+
