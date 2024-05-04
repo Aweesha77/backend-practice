076c865c7e40c5ca2c70a265f197f730f062251a
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-05-04 13:19:19 +0530

    done http req without update

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -15,23 +15,54 @@ router.get('/', async(req,res) =>{
 })
 
 //getting one
-router.get('/:id', (req,res) =>{
-    res.send(req.params.id)   //req.params.id=1234 (if url is /1234
+router.get('/:id', getSubscriber,(req,res) =>{
+    res.send(req.subscriber)   //req.params.id=1234 (if url is /1234
 })
 
 //creating one
-router.post('/', (req,res) =>{
+router.post('/', async (req,res) =>{
+    const subscriber=new subscriber({
+        name:req.body.name,
+        subscribetoChannel:req.body.subscribetoChannel
+    })
+    try{
+        const newSubscriber=await subscriber.save();
+        res.status(201).json(newSubscriber);
+    }catch(error){
+        res.status(400).json({message:error.message})
+    }
 
 })
 
 //updating one
-router.patch('/:id', (req,res) =>{
-
+router.patch('/:id',getSubscriber, (req,res) =>{
+    res.send(res.subscriber.name)
 })
 
 //deleting one
-router.delete('/:id', (req,res) =>{
-
+router.delete('/:id', getSubscriber,(req,res) =>{
+    try{
+        await res.subscriber.remove().json({message:"deleted subscriber"});
+        
+    }catch(err){
+        res.send(500).json({message:err.message})
+    }
 })
+
+async function getSubscriber(req,res,next){
+    let subscriber
+    try{
+        subscriber=await subscriber.findById(req.params.id)
+        if(subscriber==null){
+            res.send(404).json({message:"Subscriber not found"})
+        }
+    }catch(err){
+    res.send(500).json({message:err.message})
+    }
+
+    res.subscriber=subscriber;
+    next();
+
+}
 
 module.exports=router;   //exporting the router
